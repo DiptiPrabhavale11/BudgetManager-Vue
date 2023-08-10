f7318c8e23667faae3a3102de7af0187aef94cc5
DiptiPrabhavale11 <d_prabhavale@u.pacific.edu>
2023-08-09 23:28:51 -0700

    Initial Project Structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppHeader/>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+// import HelloWorld from '../components/HelloWorld.vue';
+import AppHeader from '../components/AppHeader.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    // HelloWorld,
+    AppHeader,
+  },
+});
+</script>
